524c0f67aff44ed69ab815a2de51b60935192d6b
Msekoo <1084490688@qq.com>
2021-01-10 22:53:25 +0800

    fix

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,8 +3,8 @@ const assert = require('assert')
 describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/mokz/work bench/extension/FRONT-END/webpack/my-project/build-webpack/test/smoke/template/src/index/index.js')
-        assert.equal(baseConfig.entry.search, '/Users/mokz/work bench/extension/FRONT-END/webpack/my-project/build-webpack/test/smoke/template/src/search/index.js')
+        assert.equal(baseConfig.entry.index.indexOf('build-webpack/test/smoke/template/src/index/index.js') > -1, true)
+        assert.equal(baseConfig.entry.search.indexOf('build-webpack/test/smoke/template/src/search/index.js') > -1, true)
     })
    
 }) 
